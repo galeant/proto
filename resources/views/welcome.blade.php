6bb00d76709af474741c44b921b4952986524582
ilham <ilham.fadillah@gmail.com>
2020-05-08 08:49:51 +0700

    ada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,6 +85,7 @@
                 </div>
 
                 <div class="links">
+                    
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -96,5 +97,6 @@
                 </div>
             </div>
         </div>
+        <script src="{{ asset('js/script.js') }}"></script>
     </body>
 </html>
