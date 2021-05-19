52fc1f9c4b0b2890df31391a1b27150b3fcc0b93
ilham <ilham.fadillah@noxus.co.id>
2021-05-19 12:33:56 +0700

    init

---
+++ resources/views/dashboard/shared/nav-builder.blade.php
@@ -0,0 +1,115 @@
+<div class="c-sidebar-brand"><img class="c-sidebar-brand-full" src="/assets/brand/coreui-base-white.svg" width="118" height="46" alt="CoreUI Logo"><img class="c-sidebar-brand-minimized" src="assets/brand/coreui-signet-white.svg" width="118" height="46" alt="CoreUI Logo"></div>
+ <ul class="c-sidebar-nav ps ps--active-y">
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-find-in-page') }}"></use>
+          </svg>
+          Dashboard
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-check-alt') }}"></use>
+          </svg>
+          Validasi
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-image-broken') }}"></use>
+          </svg>
+          Anomali
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item c-sidebar-nav-dropdown"><a
+          class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-book') }}"></use>
+          </svg> Report</a>
+       <ul class="c-sidebar-nav-dropdown-items">
+          <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
+                   class="c-sidebar-nav-icon"></span> Billing</a></li>
+          <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
+                   class="c-sidebar-nav-icon"></span> Delivery</a></li>
+          <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="#"><span
+                   class="c-sidebar-nav-icon"></span> Provider Performance</a></li>
+       </ul>
+    </li>
+    <li class="c-sidebar-nav-title">Master Data</li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-user') }}"></use>
+          </svg>
+          Customer
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-sitemap') }}"></use>
+          </svg>
+          Region
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-map') }}"></use>
+          </svg>
+          Area
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-dialpad') }}"></use>
+          </svg>
+          Meter
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-fingerprint') }}"></use>
+          </svg>
+          Parameter
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-braille') }}"></use>
+          </svg>
+          Anomali
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-compass') }}"></use>
+          </svg>
+          Role
+       </a>
+    </li>
+    <li class="c-sidebar-nav-item">
+       <a class="c-sidebar-nav-link" href="#">
+          <svg class="c-sidebar-nav-icon">
+             <use xlink:href="{{ url('/icons/sprites/free.svg#cil-lock-locked') }}"></use>
+          </svg>
+          User
+       </a>
+    </li>
+    {{-- <div class="ps__rail-x" style="left: 0px; bottom: 0px;">
+                <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+             </div>
+             <div class="ps__rail-y" style="top: 0px; height: 548px; right: 0px;">
+                <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 402px;"></div>
+             </div> --}}
+ </ul>
+ <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent"
+    data-class="c-sidebar-minimized"></button>
+ </div>
